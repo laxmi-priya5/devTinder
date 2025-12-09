40c314de93e6ba4bfb6fd5f498edf663c12728c9
Laxmipriya <laxmi.priyar2005@gmail.com>
2025-12-10 01:49:47 +0530

    change file

---
+++ readme.md
@@ -0,0 +1,7 @@
+1- npm init
+2- npm i express
+3-npm i -g nodemon(dont run again and again) // if already the use - sudo npm i -g nodemon
+
+.........note.....
+
+- order of the routes matter
--- src/app.js
+++ src/app.js
@@ -2,9 +2,24 @@ const express = require("express");
 
 const app = express();
 
+app.use("/hello/2",(req , res)=>{
+    res.send("welcome2  from the server");
+})
+
 app.use("/hello",(req , res)=>{
     res.send("welcome  from the server");
 })
+
+
+
+app.use("/test",(req,res)=>{
+    res.send("hello hello hello")
+})
+
+app.use("/",(req , res)=>{
+    res.send("welcome");
+})
+
 
 app.listen(3030 , ()=>console.log('listen to the port'));
 
