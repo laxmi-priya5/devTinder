96c982e74282857fd6878e013abdecd9ec581fe6
Laxmipriya <laxmi.priyar2005@gmail.com>
2025-12-17 23:51:53 +0530

    add update delete

--- src/app.js
+++ src/app.js
@@ -5,20 +5,60 @@ const app = express();
   
 const User = require("./models/user")
 
+app.use(express.json());
 
-const user1 = {
-    firstName:"Subha",
-    lastname:"Rout",
-    emailId:"subha@rout.com",
-    password:"laxmi@123"
-}
+    
 app.post("/signup",async (req , res)=>{
-   const user = new User(user1);
-   await user.save();
-   res.send("added successfully");
+    
+   const user = new User(req.body);
+   try{
+        await user.save();
+        res.send("added successfully");
+   }catch(error){
+    // res.status(400).send("error in adding user");
+     res.status(400).send(error.message);
+   }
+
 })
 
+app.get("/user",async(req,res)=>{
+    const userEmail = req.body.emailId;
+    try{
+           const users = await User.find({emailId:userEmail})
+           if(users.length>0){res.send(users);}
+           else{res.status(404).send("user not found");}
 
+    } 
+    catch{
+        res.status(400).send("error in fetching user");
+    }
+
+    
+})
+
+app.delete("/user",async(req,res)=>{
+    const userId = req.body.Id;
+    try{
+        await User.findByIdAndDelete({_id:userId});
+        res.send("user deleted successfully");
+    }
+    catch{
+        res.status(400).send("error in deleting user");
+    }
+})
+
+app.patch("/user",async(req,res)=>{
+    const userId = req.body.Id;
+    const updateData = req.body;
+    try{
+       const user =  await User.findByIdAndUpdate({_id:userId},updateData,{returnDocument:"before"});
+        res.send("user updated successfully");
+        console.log(user);
+    }
+    catch{
+        res.status(400).send("error in updating user");
+    }
+})
 
 
 connectDB()
--- src/models/user.js
+++ src/models/user.js
@@ -2,16 +2,22 @@ const mongoose = require("mongoose");
 
 const userSchema =new mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true
     },
     lastName:{
-        type:String
+        type:String,
+        required:true
     },
     emailId:{
-        type:String
+        type:String,
+        required:true,
+        unique:true 
     },
     password:{
-        type:String
+        type:String,
+        required:true,
+        
     },
     age:{
         type:Number
