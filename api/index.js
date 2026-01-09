96a3fd364bf8733fe57f38778ab2a7a4e0a178a8
Laxmipriya <laxmi.priyar2005@gmail.com>
2026-01-09 15:24:56 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -1,11 +1,3 @@
-// const app = require("../src/app");
-
-// module.exports = app;
-// index.js   (place in ROOT of your project - same level as package.json)
-
-const app = require("./src/app");
-
-// VERY IMPORTANT: Do NOT add app.listen() here!
-// Vercel will handle it automatically in serverless environment
+const app = require("../src/app");
 
 module.exports = app;
