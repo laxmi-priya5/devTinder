cba58874954b879646e34b56fd15139c05c7493e
Laxmipriya <laxmi.priyar2005@gmail.com>
2026-01-09 14:35:31 +0530

    add chnages

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
-const app = require("../src/app");
+// const app = require("../src/app");
+
+// module.exports = app;
+// index.js   (place in ROOT of your project - same level as package.json)
+
+const app = require("./src/app");
+
+// VERY IMPORTANT: Do NOT add app.listen() here!
+// Vercel will handle it automatically in serverless environment
 
 module.exports = app;
